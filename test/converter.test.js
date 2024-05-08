7ca97597444cbb132558ec8b4707db45390e5aa3
FSFahim <farhanshahriar3728@gmail.com>
2024-05-09 02:55:06 +0600

    Init

--- test/converter.test.js
+++ test/converter.test.js
@@ -1,19 +1,22 @@
-const { WeightConverter } = require('../src/converter'); // Update 'yourFileName.js' with the actual file name
+const { WeightConverter } = require("../src/converter"); // Update 'yourFileName.js' with the actual file name
 
-describe('WeightConverter', () => {
-    it('converts ounces to pounds and tons correctly', () => {
-        expect(WeightConverter("ounces", 4)).toEqual(["0.25 pounds", "0.00 tons"]);
-    });
+describe("WeightConverter", () => {
+  it("converts ounces to pounds and tons correctly", () => {
+    expect(WeightConverter("ounces", 4)).toEqual(["0.25 pounds", "0.00 tons"]);
+  });
 
-    it('converts pounds to ounces and tons correctly', () => {
-        expect(WeightConverter("pounds", 1)).toEqual(["16.00 ounces", "0.00 tons"]);
-    });
+  it("converts pounds to ounces and tons correctly", () => {
+    expect(WeightConverter("pounds", 1)).toEqual(["16.00 ounces", "0.00 tons"]);
+  });
 
-    it('converts tons to pounds and ounces correctly', () => {
-        expect(WeightConverter("tons", 1)).toEqual(["2000.00 pounds", "32000.00 ounces"]);
-    });
+  it("converts tons to pounds and ounces correctly", () => {
+    expect(WeightConverter("tons", 1)).toEqual([
+      "2000.00 pounds",
+      "32000.00 ounces",
+    ]);
+  });
 
-    it('returns "wrong input" for invalid measurement', () => {
-        expect(WeightConverter("invalid", 10)).toEqual("wrong input");
-    });
+  it('returns "wrong input" for invalid measurement', () => {
+    expect(WeightConverter("invalid", 10)).toEqual("wrong input");
+  });
 });
